9f9a0694ac9ba705ed34598430b10c4d153bfbf9
Plamada Alin <alinplamada@gmail.com>
2024-07-06 14:08:45 +0300

    Added a test that checks if the google images search works

--- cypress/e2e/testeGoogle/test1.cy.js
+++ cypress/e2e/testeGoogle/test1.cy.js
@@ -1,10 +1,18 @@
-describe('test google',()=>{
+describe('Teste google',()=>{
     it('functioneza un search', ()=> {
         cy.visit('https://google.com');
         cy.get('#L2AGLb').click();
-        cy.get('.gLFyf').type('vlog de it').type('{enter}');
+        cy.get('.gLFyf').type('testare automata').type('{enter}');
         cy.get('.WE0UJf').should('exist');
     })
+    it('verifies Google image search', () => {
+        cy.visit('https://google.com');
+        cy.get('#L2AGLb').click();
+        cy.get('.gLFyf').type('pisici').type('{enter}');
+        cy.wait(2000);
+        cy.contains('Imagini').click();
+        cy.url().should('include', '/search?').and('include', 'q=pisici');
+    });
    
 
 });
